e5282db4404fd09be9958cfe52ac9db4ba1b5b55
emna-mana <enmabenamor@gmail.com>
2025-12-02 10:26:45 +0100

    sec

--- rh_service/wsgi.py
+++ rh_service/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rh_service.settings')
 
+# Create the WSGI application
 application = get_wsgi_application()
